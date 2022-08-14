104db97d09ad5f3d0e3dd7199e947a0e434b8c7e
康 <1336738527@qq.com>
2022-08-14 20:16:08 +0800

    two

---
+++ node-demo/node-app/server.js
@@ -0,0 +1,24 @@
+// 引入express
+const express = require("express");
+
+// 引入mongoose
+const mongoose = require("mongoose");
+// 实例化
+const app = express();
+// connect database
+const dbURL = "mongodb://127.0.0.1:27017/nodeapi";
+mongoose.connect(dbURL)
+
+    .then(() => console.log("connect database"))
+    .catch(err => console.log(err))
+
+// 设置路由
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+// 设置端口号
+const port = process.env.PORT || 5000;
+// 监听
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
